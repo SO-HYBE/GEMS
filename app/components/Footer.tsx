12b31a5c818d2486320a010e203ff283a2051054
Sohybe <sohaibibrahim14@gmail.com>
2024-09-28 23:13:08 +0300

    Added links to the footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import logo from "../../public/GEMS-logo-v.png";
+import phoneSvg from "../../public/phone-svgrepo-com.svg";
 
 export default function Footer() {
   return (
@@ -34,25 +35,24 @@ export default function Footer() {
 
       <ul className="flex justify-center mt-5 space-x-5">
         <li>
-          <a href="/" className="text-[#6c3483] ">
+          <a
+            href="tel:+201272857888"
+            className="text-[#6c3483] hover:text-[#e0c6f5] duration-200"
+          >
             <svg
-              className="w-6 h-6 hover:text-[#cd90ff] duration-200"
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6"
+              viewBox="0 -960 960 960"
               fill="currentColor"
-              viewBox="0 0 24 24"
-              aria-hidden="true"
             >
-              <path
-                fillRule="evenodd"
-                d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                clipRule="evenodd"
-              ></path>
+              <path d="M796-120q-119 0-240-55.5T333-333Q231-435 175.5-556T120-796q0-18.86 12.57-31.43T164-840h147.33q14 0 24.34 9.83Q346-820.33 349.33-806l26.62 130.43q2.05 14.9-.62 26.24-2.66 11.33-10.82 19.48L265.67-530q24 41.67 52.5 78.5T381-381.33q35 35.66 73.67 65.5Q493.33-286 536-262.67l94.67-96.66q9.66-10.34 23.26-14.5 13.61-4.17 26.74-2.17L806-349.33q14.67 4 24.33 15.53Q840-322.27 840-308v144q0 18.86-12.57 31.43T796-120ZM233-592l76-76.67-21-104.66H187q3 41.66 13.67 86Q211.33-643 233-592Zm365.33 361.33q40.34 18.34 85.84 29.67 45.5 11.33 89.16 13.67V-288l-100-20.33-75 77.66ZM233-592Zm365.33 361.33Z" />
             </svg>
           </a>
         </li>
         <li>
-          <a href="/" className="text-[#6c3483] ">
+          <a href="https://www.instagram.com/gemswomen/" className="text-[#6c3483] hover:text-[#e0c6f5] duration-200">
             <svg
-              className="w-6 h-6 hover:text-[#e0c6f5] duration-200"
+              className="w-6 h-6"
               fill="currentColor"
               viewBox="0 0 24 24"
               aria-hidden="true"
@@ -66,18 +66,14 @@ export default function Footer() {
           </a>
         </li>
         <li>
-          <a href="/" className="text-[#6c3483] ">
+          <a href="mailto:gems.10162628@gmail.com" className="text-[#6c3483] hover:text-[#e0c6f5] duration-200">
             <svg
-              className="w-6 h-6 hover:text-[#cf94ff] duration-200"
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6"
+              viewBox="0 -960 960 960"
               fill="currentColor"
-              viewBox="0 0 24 24"
-              aria-hidden="true"
             >
-              <path
-                fillRule="evenodd"
-                d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                clipRule="evenodd"
-              ></path>
+              <path d="M146.67-160q-27 0-46.84-19.83Q80-199.67 80-226.67v-506.66q0-27 19.83-46.84Q119.67-800 146.67-800h666.66q27 0 46.84 19.83Q880-760.33 880-733.33v506.66q0 27-19.83 46.84Q840.33-160 813.33-160H146.67ZM480-454.67 146.67-670v443.33h666.66V-670L480-454.67Zm0-66.66 330.67-212H150l330 212ZM146.67-670v-63.33V-226.67-670Z" />
             </svg>
           </a>
         </li>
